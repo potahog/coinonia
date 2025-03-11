a3935d27384c4205865f03586292203c990605c9
Potahog <potahog@gmail.com>
2025-03-11 13:18:02 +0900

    application.yml 추가 및 기본 세팅 ------------------------------- port default: 8080
    datasource modify element
    url: your database url
    username: your setting data base access user name
    password: your setting data base access password
    driver-class-name: use sql driver class name
    
    data.redis modify
    host: your redis url
    port: your redis port
    
    jwt:
    secret: your jwt key
    expiration: jwt expiration setting.
    
    security:
     client-id: your-google-client-id
     client-secret: your-google-client-secret

--- flow/build.gradle.kts
+++ flow/build.gradle.kts
@@ -28,6 +28,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5") // JSON 처리를 위해 필요
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
