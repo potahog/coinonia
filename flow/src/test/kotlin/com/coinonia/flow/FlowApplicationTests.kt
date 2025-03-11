ac1d1873814f0a18746ae9be7f6148064b94de64
Potahog <potahog@gmail.com>
2025-03-11 12:27:53 +0900

    docker0compose.yml 및 dockerfile 수정

---
+++ flow/src/test/kotlin/com/coinonia/flow/FlowApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coinonia.flow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
